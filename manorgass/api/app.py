1c1041fa327a6805f7808f0e2f5e9f9ab3bd4da6
cardone <manorgass@gmail.com>
2022-10-06 16:15:19 +0900

    feat: add sign-up api

--- manorgass/api/app.py
+++ manorgass/api/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+app.id_count = 1
+app.users = {}
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user                = request.json
+    new_user["id"]          = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count            = app.id_count + 1
+
+    return jsonify(new_user)
