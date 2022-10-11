88d435c897e83cb47f4e74b8941bfbea4161b8da
Jooyeon Hwang <zooyouny@gmail.com>
2022-10-12 01:08:26 +0900

    Add timeline

--- zooyouny/Chapter05/app.py
+++ zooyouny/Chapter05/app.py
@@ -76,3 +76,15 @@ def unfollow():
     user.setdefault("follow", set()).discard(user_id_to_follow)
 
     return jsonify(user)
+
+
+@app.route("/timeline/<int:user_id>", methods=["GET"])
+def timeline(user_id):
+    if user_id not in app.users:
+        return "사용자가 존재하지 않습니다", 400
+
+    follow_list = app.users[user_id].get("follow", set())
+    follow_list.add(user_id)
+    timeline = [tweet for tweet in app.tweets if tweet["user_id"] in follow_list]
+
+    return jsonify({"user_id": user_id, "timeline": timeline})
