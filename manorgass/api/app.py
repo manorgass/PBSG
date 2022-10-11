e54747dae3e85e8307d1626fd94aa9252aa4074b
cardone <manorgass@gmail.com>
2022-10-11 13:38:01 +0900

    add Tweet API

--- manorgass/api/app.py
+++ manorgass/api/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-app.id_count = 1
-app.users = {}
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
+
+app.id_count = 1
+app.users = {}
 
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
@@ -16,3 +17,24 @@ def sign_up():
     app.id_count            = app.id_count + 1
 
     return jsonify(new_user)
+
+app.tweets = []
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet   = payload['tweet']
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+
+    if len(tweet) > 300:
+        return '300자를 초과해습니다', 400
+    
+    # user_id = int(payload['id'])
+    app.tweets.append({
+        'user_id'   :   user_id,
+        'tweet'     :   tweet
+    })
+
+    return '', 200
