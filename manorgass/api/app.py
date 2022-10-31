bed69c6923ade14e5f951a43d2d56d14009a9f76
JungWon Kim <manorgass@gmail.com>
2022-10-31 15:43:33 +0900

    feat: DB 연동 설정코드 추가 (#8)
    * feat: add db config
    
    * miniter table 생성 쿼리파일 추가
    
    * feat: Flask 생성자 메서드 override

--- manorgass/api/app.py
+++ manorgass/api/app.py
@@ -1,5 +1,21 @@
 from flask import Flask, request, jsonify
 from flask.json import JSONEncoder
+from sqlalchemy import create_engine, text
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database = create_engine(
+        app.config['DB_URL'], encoding='utf-8', max_overflow=0)
+    app.database = database
+
+    return app
 
 
 class CustomJSONEncoder(JSONEncoder):
